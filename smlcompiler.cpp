b4911872089fcf00dd2876c5273a9351a1bea465
David Croft <dcroft2001@gmail.com>
2019-11-04 13:24:43 -0500

    Fixed it

--- smlcompiler.cpp
+++ smlcompiler.cpp
@@ -17,30 +17,45 @@ const int smlBufferSize(100); //Sets the size of the SML buffer array
 // Declares the symbol, type, and location columns of the symbol table
 array<string,symbolTableSize> symbolColumn;
 array<string,symbolTableSize> typeColumn;
-array<string,symbolTableSize> locColumn;
+array<int,symbolTableSize> locColumn;
 
 // Declares the array that acts as the buffer that contains the translated SML code
 array<int,smlBufferSize> smlBuffer;
 
 //Declares data, symbol table, and buffer counters
-symbolCOunt = 0;
-bufferCount = 0;
-dataCount = 99;
+int symbolCount = 0;
+int bufferCount = 0;
+int dataCount = 99;
 
-int var(string vname){
-	for(int i = 0; i < symbolTableSize; i++){
-		
+int var(string vname, bool create){
+	for(int i = 0; i < symbolTableSize; i++)
+	{
+		if(symbolColumn[i] == vname)
+		{
+			return locColumn[i];
+		} 
 	}
+	if(!create)
+	{
+		cerr<<"Variable " + vname + " does not exist.";
+	}
+	symbolColumn[symbolCount] = vname;
+	typeColumn[symbolCount] = "V";
+	locColumn[symbolCount] = dataCount;
+	symbolCount++;
+	dataCount--;
+	return dataCount+1;
 }
 
 int main()
 {
 	// *CONSTANTS*
-	const string FILE_NAME = "smlcompileroutput.txt"; // Sets the file name for the compiler to take in
+	const string IN_FILE_NAME = "smlcompilerinput.txt"; // Sets the file name for the compiler to take in
+	const string OUT_FILE_NAME = "smlcompileroutput.txt";
 
 	// *MAIN CODE*
 	//Opens the file to be read as "codeFile", and if unable to open, exits the program with error message
-	ifstream codeFile(FILE_NAME,ios::in);
+	ifstream codeFile(IN_FILE_NAME,ios::in);
 	if(!codeFile.is_open())
 	{
 		cerr<<"Unable to open file."<<endl;
@@ -51,32 +66,66 @@ int main()
 	string p1; // First part of string
 	string p2; // Second part of string
 	string p3; // Third part of string
-	codeFile>>p1;
-	codeFile>>p2;
-	codeFile>>p3;
+	string line;
+	//codeFile>>line;
+	
 	int index = 0;
 	while(!codeFile.eof())
 	{
-		switch(p2){
-			case input:
-				{
-				symbolColumn[symbolCount] = p1;
-				typeColumn[symbolCount] = "L";
-				locColumn[symbolCount] = bufferCount;
-				smlBuffer[bufferCount] = 1000+var(p3);
-				symbolCount++;
-				symbolColumn[symbolCount] = p3;
-				typeColumn[symbolCount] = "V"
-				locColumn[symbolCount] = dataCount;
-				bufferCount++;
-				dataCount--;
-				}
-				break;
+		getline(codeFile, line);
+		stringstream ss(line);
+		ss >> p1;
+		ss >> p2;
+		getline(ss, p3);
+		if(p2 == "input")
+		{
+			symbolColumn[symbolCount] = p1;
+			typeColumn[symbolCount] = "L";
+			locColumn[symbolCount] = bufferCount;
+			symbolCount++;
+			smlBuffer[bufferCount] = 1000+var(p3, true);
+			bufferCount++;
 		}
-		cout<<command<<endl;
+		else if(p2 == "print")
+		{
+			symbolColumn[symbolCount] = p1;
+			typeColumn[symbolCount] = "L";
+			locColumn[symbolCount] = bufferCount;
+			symbolCount++;
+			smlBuffer[bufferCount] = 1100+var(p3, false);
+			bufferCount++;	
+		}
+		else if(p2 == "end")
+		{
+			symbolColumn[symbolCount] = p1;
+			typeColumn[symbolCount] = "L";
+			locColumn[symbolCount] = bufferCount;
+			symbolCount++;
+			smlBuffer[bufferCount] = 4300;
+			bufferCount++;
+		}
 		index++;
-		codeFile>>command;
 	}
-	codeFile.close();
 
+	codeFile.close();
+	
+	ofstream outFile(OUT_FILE_NAME,ios::in);
+	if(!outFile.is_open())
+	{
+		cerr<<"Unable to open file."<<endl;
+		exit(EXIT_FAILURE);
+	}
+
+	for(int i = 0; i < smlBufferSize; i++){
+		if(smlBuffer[i] == 0)
+			break;
+		outFile << smlBuffer[i] << "\n";
+	}
+	outFile.close();
 }
+
+
+
+
+
+
